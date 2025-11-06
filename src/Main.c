e25a6435cb2c7fd3129d92036f6c6d2493171065
codeleaded <codeleaded@gmail.com>
2025-11-06 18:18:19 +0100

    update

--- src/Main.c
+++ src/Main.c
@@ -1,20 +1,39 @@
-#include "./Console_Engine.h"
+#include "/home/codeleaded/System/Static/Library/ConsoleEngine.h"
 
-void Setup(){
+Vec2 p = {50.0f,50.0f};
+Vec2 e = {100.0f,100.0f};
+
+void C_Setup(Console* c){
+
 }
+void C_Update(Console* c){
+    if(Console_Stroke(c,'W').DOWN){
+        p.y -= 50.0f * c->ElapsedTime;
+    }
+    if(Console_Stroke(c,'S').DOWN){
+        p.y += 50.0f * c->ElapsedTime;
+    }
+    if(Console_Stroke(c,'A').DOWN){
+        p.x -= 50.0f * c->ElapsedTime;
+    }
+    if(Console_Stroke(c,'D').DOWN){
+        p.x += 50.0f * c->ElapsedTime;
+    }
 
-void Update(void* c_void){
-    Console* c = (Console*)c_void;
-    SetColor(c,FOREGROUND_GREEN);
-    RenderPoint(c,(Vf2d){ 0.0f,10.0f },L'@');
-    RenderPoint(c,(Vf2d){ 2.0f,11.0f },L'@');
-    RenderPoint(c,(Vf2d){ 4.0f,12.0f },L'@');
-    RenderPoint(c,(Vf2d){ 6.0f,13.0f },L'@');
-    RenderPoint(c,(Vf2d){ 8.0f,14.0f },L'@');
+    e = Console_Mouse(c);
+
+    Console_Clear(c,CPIXEL_SOLID,FG_BLACK);
+
+    Console_RenderTriangleWire(c,e,(Vec2){30.0f,30.0f},p,(CPixel){CPIXEL_SOLID,FG_YELLOW},1.0f);
+    Console_RenderTriangleWire(c,e,(Vec2){10.0f,50.0f},p,(CPixel){CPIXEL_SOLID,FG_BLUE},1.0f);
+}
+void C_Delete(Console* c){
+
 }
 
 int main(){
-    Console c = CreateConsole(160.0f,80.0f,Setup,Update);
-    Start(&c);
+    Console c;
+    if(Console_Create(&c,L"Console Game Test",200,150,8,8,C_Setup,C_Update,C_Delete))
+        Console_Start(&c);
     return 0;
 }
